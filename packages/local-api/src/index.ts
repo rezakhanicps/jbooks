cfc5dbf81846b1fbc81cfa269632b64092a28050
reza khani <43088060+rezakhani1376@users.noreply.github.com>
2022-10-20 17:23:01 +0330

    Build a Production Bundle

--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,6 +1,27 @@
 import express from 'express';
-export const serve = (port: number, filename: string, dir: string) => {
+import { createProxyMiddleware } from 'http-proxy-middleware';
+import path from 'path';
+
+export const serve = (
+    port: number,
+    filename: string,
+    dir: string,
+    useProxy: boolean
+) => {
     const app = express();
+
+    if (useProxy) {
+        app.use(
+            createProxyMiddleware({
+                target: 'http://localhost:3000',
+                ws: true,
+                logLevel: 'silent',
+            })
+        );
+    } else {
+        const packagePath = require.resolve('local-client/build/index.html');
+        app.use(express.static(path.dirname(packagePath)));
+    }
 
     return new Promise<void>((resolve, reject) => {
         app.listen(port, resolve).on('error', reject);
