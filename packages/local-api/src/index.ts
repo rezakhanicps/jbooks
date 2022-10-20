155f57edfc0d1716f62229c114f4cc81e1d0668b
reza khani <43088060+rezakhani1376@users.noreply.github.com>
2022-10-20 12:16:43 +0330

    Creating The CLI

--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,3 +1,5 @@
-export default () => {
-    console.log('server is listening');
+export const serve = (port: number, filename: string, dir: string) => {
+    console.log('alksgj', port);
+    console.log('lksajkdsa', filename);
+    console.log('askdljf', dir);
 };
